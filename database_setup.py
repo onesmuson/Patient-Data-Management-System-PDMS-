e4fd4bcf606afcb60de403b61ff58afa6bbe33cd
onesmuson <masooonesmus1@gmail.com>
2025-11-09 00:37:50 +0300

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -2,8 +2,12 @@ from app import app
 from models import db, User
 
 with app.app_context():
+    # Create database tables if they don't exist
     db.create_all()
+
+    # Check if admin exists
     if not User.query.filter_by(username='admin').first():
+        # Add default admin user
         admin = User(username='admin', password='admin123')
         db.session.add(admin)
         db.session.commit()
