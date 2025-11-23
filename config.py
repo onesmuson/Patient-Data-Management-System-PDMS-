b0e56dee1e5782f2d23518bfc1bcf3941215af33
onesmuson <masooonesmus1@gmail.com>
2025-11-24 00:24:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
-    
-    # PostgreSQL database (Render)
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://patient_data_management_system_db_4jrj_user:"
-        "mQpr9rAQvV1pKYBCWhyalT1AB5sad0iM@dpg-d4fibsggjchc73e3r1tg-a.oregon-postgres.render.com/"
-        "patient_data_management_system_db_4jrj"
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_should_be_random'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://patient_data_management_system_db_4jrj_user:mQpr9rAQvV1pKYBCWhyalT1AB5sad0iM@dpg-d4fibsggjchc73e3r1tg-a.oregon-postgres.render.com/patient_data_management_system_db_4jrj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
